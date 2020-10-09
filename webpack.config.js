f622da9266439ccc5ca5b4393525ae4a3113235a
Oleg Kadyrov <abathojalyk@gmail.com>
2020-10-09 12:07:35 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 3000
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+
+}
