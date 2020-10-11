1247a8e055864a0cfb049e191044ac385d05a6ee
Oleg Kadyrov <abathojalyk@gmail.com>
2020-10-11 12:00:04 -0500

    code refactoring

--- src/model.js
+++ src/model.js
@@ -1,11 +1,18 @@
 import image from './assets/image.png'
 import {TextBlock, TitleBlock, ImageBlock, TextColumnsBlock} from './classes/blocks'
+import {css} from "./utils";
 
 
 export const model = [
     new TitleBlock( 'Website builder in JavaScript', {
         tag: 'h2',
-        styles: 'background: linear-gradient(to right, #ff0099, #493240);color: #fff;padding: 1.5rem;text-align: center;'
+        styles: css({
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            padding: '1.5rem',
+            'text-align': 'center'
+        })
+        //styles: 'background: linear-gradient(to right, #ff0099, #493240);color: #fff;padding: 1.5rem;text-align: center;'
     }),
     new ImageBlock(  image,  {
         styles: 'padding: 2rem 0;display: flex;justify-content: center;',
